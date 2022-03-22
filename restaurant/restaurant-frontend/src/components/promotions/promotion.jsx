b84d9b5d809144ed59ab1105b5e19bd8ebd8d3dd
Carter Fuchs <carterjfuchs@gmail.com>
2022-03-22 17:35:46 -0600

    Restaurant UI: Added buttons to delete promotions on the promotions page

--- restaurant/restaurant-frontend/src/components/promotions/promotion.jsx
+++ restaurant/restaurant-frontend/src/components/promotions/promotion.jsx
@@ -1,37 +1,81 @@
 import React from "react";
-import './styles.css';
-import EditDialog from './promoEditor';
+import "./styles.css";
+import EditDialog from "./promoEditor";
+import IconButton from "@material-ui/core/Button";
+import DeleteIcon from "@material-ui/icons/Delete"
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import Dialog from "@material-ui/core/Dialog";
+import Button from "@material-ui/core/Button";
 
 export default class promotion extends React.Component {
   constructor(props) {
     super();
     this.state = {
+      open: false,
       data: props.data
-    }
+    };
   }
 
   handleDialogForm = (event, newData) => {
-    console.log(event);
     this.setState({
-      data: newData
-    })
+      data: newData,
+    });
+  };
+
+  handleDelete (event) {
+    this.props.handleDelete(event, this.props.index);
+  }
+
+  handleOpen() {
+    this.setState({
+      open: true,
+      data: this.state.data
+    });
+  }
+
+  handleClose () {
+    console.log("test");
+    this.setState({
+      open: false,
+      data: this.state.data
+    });
+    console.log(this.state.open);
   }
 
   render() {
     return (
-      <div>
-        <table className = 'promotion'>
-          <tbody>
-            <tr>
-              <th>{this.state.data.title}</th>
-              <th>{this.state.data.description}</th>
-              <th>{this.state.data.category}</th>
-              <th>{this.state.data.priceReduction}% off</th>
-              <th><EditDialog handleSubmitParent={this.handleDialogForm.bind(this)} data={this.state.data}/></th>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <tr>
+        <th>{this.state.data.title}</th>
+        <th>{this.state.data.description}</th>
+        <th>{this.state.data.category}</th>
+        <th>{this.state.data.priceReduction}% off</th>
+        <th>
+          <EditDialog
+            handleSubmitParent={this.handleDialogForm.bind(this)}
+            data={this.state.data}
+          />
+        </th>
+        <th>
+          <IconButton onClick={this.handleOpen.bind(this)}>
+            <DeleteIcon />
+          </IconButton>
+          <Dialog open={this.state.open} onClose={this.handleClose.bind(this)}>
+              <DialogTitle>Confirm promotion deletion</DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  Are you sure you wish to delete this promotion? This action isn't reversible.
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={this.handleDelete.bind(this)}>Confirm</Button>
+                <Button onClick={this.handleClose.bind(this)}>Cancel</Button>
+              </DialogActions>
+            </Dialog>
+        </th>
+      </tr>
     );
   }
 }
--- restaurant/restaurant-frontend/src/pages/promotions.jsx
+++ restaurant/restaurant-frontend/src/pages/promotions.jsx
@@ -1,31 +1,57 @@
+import React from 'react'
 import DirectButton from "../components/directButton";
+import promotion from '../components/promotions/promotion';
 import Promotion from "../components/promotions/promotion";
 
 export default function Promotions() {
   //get list of promotions active at the restaurant that's logged in, via an API call (for now just use this example object)
-  const promotionList = [
+  const startingPromotionList = [
     {
       title: "Example promotion",
       description: "Burgers 40% off",
       category: "Burgers",
       priceReduction: "40",
+      key: 0
     },
     {
       title: "Example promotion 2",
       description: "Drinks 80% off",
       category: "Drinks",
       priceReduction: "80",
+      key: 1
     },
   ];
+
+  const [promotionList, setPromotionList] = React.useState(startingPromotionList);
+
+  const handleDelete = (event, index) => {
+    //Add an API call to delete the item from the database when this event is called
+    console.log(index);
+    const newList = [...promotionList]
+    newList.splice(index, 1);
+    setPromotionList(newList);
+  }
 
   return (
     //iterate through list of promotions, render each as a promotion component
     <div>
       <h1>Current promotions:</h1>
-      {promotionList.map((obj, i) => {
-        return <Promotion data={obj} key={i} />;
-      })}
-      <DirectButton route='/home' text='Cancel' />
+      <table>
+        <thead>
+          <tr>
+            <th>Name of promotion</th>
+            <th>Promotion description</th>
+            <th>Discounted category</th>
+            <th>Discount percentage</th>
+          </tr>
+        </thead>
+        <tbody>
+          {promotionList.map((obj, i) => {
+            return <Promotion data={obj} index={i} key={obj.key} handleDelete={handleDelete}/>;
+          })}
+        </tbody>
+      </table>
+      <DirectButton route="/home" text="Cancel" />
     </div>
   );
 }
